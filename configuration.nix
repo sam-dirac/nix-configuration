d01cd15fa1e5f2ffb21a97e571e6e1083e9590cc
Sam Title <sam@diracinc.com>
2025-03-18 16:25:16 -0400

    Adding OBS for better screen capture

--- configuration.nix
+++ configuration.nix
@@ -221,4 +221,13 @@
   services.atuin = {
     enable = true;
   };
+  
+  programs.obs-studio = {
+    enable = true;
+    plugins = with pkgs.obs-studio-plugins; [
+      wlrobs
+      obs-backgroundremoval
+      obs-pipewire-audio-capture
+    ];
+  };
 }
